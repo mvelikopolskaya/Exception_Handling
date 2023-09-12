23be076051b10967f9da921d7370e713571504ca
Cindrom1 <cindrof@gmail.com>
2023-09-12 12:19:48 +0300

    changed Task_4

--- Lesson_2/src/Task_4.java
+++ Lesson_2/src/Task_4.java
@@ -7,19 +7,17 @@ import java.util.EmptyStackException;
 import java.util.Scanner;
 
 public class Task_4 {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         noEmptyStrings();
     }
 
-    public static void noEmptyStrings() throws Exception {
+    public static void noEmptyStrings() {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("введите строку");
+        System.out.println("Введите строку");
         String line = scanner.nextLine();
         if (line.isEmpty()){
             System.out.println("Нельзя вводить пустые строки");
-            Exception EmptyStackException = null;
-            throw EmptyStackException;
-
+            throw new EmptyStackException();
         }
     }
 }
